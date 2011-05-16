fa020147df1386bfadaa72e91c6076e52d1afdbc
David Thole <dthole@gmail.com>
2011-05-16 09:32:27 -0500

    Copying a bit more code over from the wordnet app, to disable caching and all so development is easier

--- lib/cl-gene-searcher/db-gene-searcher.lisp
+++ lib/cl-gene-searcher/db-gene-searcher.lisp
@@ -1,2 +1,15 @@
+; db-gene-searcher.lisp
+;
+; Requires on the included sqlite database to work correctly.
 (in-package :cl-gene-searcher)
+
+;;;; Disable caching, mostly for debugging at this point ;;;;
+(setf clsql:*cache-table-queries-default* nil)
+
+;;;;;; ENABLE SYNTAX READER ;;;;;;;
+; Note, ran into some issues with this at times, run:
+; (clsql:locally-disable-sql-reader-syntax) to disable syntax, then again to reenable
+;
+(clsql:locally-disable-sql-reader-syntax)
+(clsql:locally-enable-sql-reader-syntax)
 
