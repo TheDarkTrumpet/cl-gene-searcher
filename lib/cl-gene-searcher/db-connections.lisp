ff08117e06cd163a6d04eabda12b5841a5a1e28f
David Thole <dthole@gmail.com>
2011-05-16 09:05:29 -0500

    Added the database connections to the file.  Currently, only SQLITE is supported, but eventually the addition of MySQL/MSSQL is possible.

--- lib/cl-gene-searcher/db-connections.lisp
+++ lib/cl-gene-searcher/db-connections.lisp
@@ -1,2 +1,27 @@
+;  This file only has the database connection information.
+;  Currently only SQLITE is available.
+
 (in-package :cl-gene-searcher)
 
+(defclass db-connection ()
+  ((type :initarg :type :accessor db-type))
+  (:documentation "The foundation datbase connection class, that the others will inherit"))
+
+(defclass db-connection-sqlite (db-connection)
+  ((type :initform "sqlite3" :reader db-type)
+   (path :initarg :path :accessor path))
+  (:documentation "The specific connection object for SQLite"))
+
+(defgeneric connect-db (db-connection &optional make-default)
+  (:documentation "Generic method for which the various database types will be supported"))
+
+(defmethod connect-db ((db db-connection-sqlite) &optional make-default)
+  (connect (list 
+	    (path db))
+	   :database-type :sqlite3
+	   :make-default make-default))
+
+(defun disconnect-db (db)
+  (when (eql (database-state db) :open)
+    (disconnect :database db))
+  T)
