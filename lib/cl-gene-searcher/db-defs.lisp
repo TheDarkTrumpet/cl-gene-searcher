58a8a08d6f0baf32f41fd4cfefcbbe2757ae9b7b
David Thole <dthole@gmail.com>
2011-05-20 16:10:00 -0500

    Adding the DGV view class

--- lib/cl-gene-searcher/db-defs.lisp
+++ lib/cl-gene-searcher/db-defs.lisp
@@ -12,9 +12,18 @@
    (stop_region :accessor stop_region :type integer :initarg :stop_region)
    (strand :accessor strand :type (string 1) :initarg :strand)
    (platform :accessor platform :db-kind :join
-	     :db-info (:join-class genePlatform
-				   :home-key platform
-				   :foreign-key id
-				   :retrieval :immediate
-				   )))
+	     :db-info ( :join-class genePlatform
+				    :home-key platform
+				    :foreign-key id
+				    :retrieval :immediate
+				    :set nil
+				    )))
   (:base-table genes))
+
+(def-view-class dgv ()
+  ((id :type integer :reader id :db-kind :key)
+   (chr :accessor chr :type (string 5) :initarg :chr)
+   (start_region :accessor start_region :type integer :initarg :start_region)
+   (stop_region :accessor stop_region :type integer :initarg :stop_region)
+   (strand :accessor strand :type (string 1) :initarg :strand))
+  (:base-table dgv))
