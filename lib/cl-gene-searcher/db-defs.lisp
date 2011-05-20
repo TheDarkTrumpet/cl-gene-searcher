31041a0d409f20a39ad0976531f91606cce52481
David Thole <dthole@gmail.com>
2011-05-20 13:42:32 -0500

    Adding the def-view-class which is the model of much of the genes table, but I still need to create a foreign key relationsip to the genePlatform

--- lib/cl-gene-searcher/db-defs.lisp
+++ lib/cl-gene-searcher/db-defs.lisp
@@ -1,1 +1,9 @@
 (in-package :cl-gene-searcher)
+
+(def-view-class genes ()
+  ((name :accessor name :type (string 255) :initarg :name)
+   (chr :accessor chr :type (string 5) :initarg :chr)
+   (start_position :accessor start_position :type integer :initarg :start_position)
+   (stop_position :accessor stop_position :type integer :initarg :stop_position)
+   (strand :accessor strand :type (string 1) :initarg :strand)
+   ))
