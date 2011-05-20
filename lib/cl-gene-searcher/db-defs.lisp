9446b8876e47c0cb56d8ae433754682d8dba8b21
David Thole <dthole@gmail.com>
2011-05-20 16:02:04 -0500

    Adding in the genePlatform class, also added in the fk relationship, but it doesn't work yet, for genes

--- lib/cl-gene-searcher/db-defs.lisp
+++ lib/cl-gene-searcher/db-defs.lisp
@@ -1,9 +1,20 @@
 (in-package :cl-gene-searcher)
 
+(def-view-class genePlatform ()
+  ((id :type integer :reader id :db-kind :key)
+   (name :accessor name :type (string 255) :initarg :name))
+  (:base-table genePlatform))
+
 (def-view-class genes ()
   ((name :accessor name :type (string 255) :initarg :name)
    (chr :accessor chr :type (string 5) :initarg :chr)
    (start_region :accessor start_region :type integer :initarg :start_region)
    (stop_region :accessor stop_region :type integer :initarg :stop_region)
-   (strand :accessor strand :type (string 1) :initarg :strand))
+   (strand :accessor strand :type (string 1) :initarg :strand)
+   (platform :accessor platform :db-kind :join
+	     :db-info (:join-class genePlatform
+				   :home-key platform
+				   :foreign-key id
+				   :retrieval :immediate
+				   )))
   (:base-table genes))
