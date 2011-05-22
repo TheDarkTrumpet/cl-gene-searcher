b6642d9666f7a004901520f8e623c63e6884b01e
David Thole <dthole@gmail.com>
2011-05-22 18:52:30 -0500

    Adding the import/exports to the cl-gene-searcher

--- lib/cl-gene-searcher/package.lisp
+++ lib/cl-gene-searcher/package.lisp
@@ -5,4 +5,7 @@
 (defpackage :cl-gene-searcher
     (:use :cl :clsql-sqlite3)
   (:import-from :clsql-sys :database-state)
-  (:import-from :clsql :def-view-class :disconnect :connect))
+  (:import-from :clsql :def-view-class :disconnect :connect)
+  (:export :setDatabasePath :getDatabasePath
+	   :query-gene-by-name :query-ucsc-gene-by-range
+	   :query-gene-by-range :query-dgv-by-range))
